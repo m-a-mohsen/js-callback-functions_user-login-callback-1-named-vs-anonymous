6f2dffc89872af76d33b7be501637e81a4fbbb89
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-11 15:42:45 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -4,5 +4,15 @@ function handleUserLogin(onSuccess) {
   // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
   onSuccess();
 }
+// create more functions
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now");
+}
 
 // Call handleUserLogin below!
+
+handleUserLogin(showWelcomeMessage)
+//arrow callback function
+handleUserLogin(() => {
+  console.log("Welcome! You are logged in now");
+})
